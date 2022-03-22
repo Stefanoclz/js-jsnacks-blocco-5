f2d8040f5242633318ea2aa878e22779398dccf9
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-22 16:41:56 +0100

    sistemato codice per aggiungere la funzione come da richiesta dell'esercizio

--- jsnack4(bonus)/script.js
+++ jsnack4(bonus)/script.js
@@ -10,13 +10,19 @@ const array2 = [1, 2, 3, 4, 5, 6]
 
 const arraySomma = [];
 
-for (let i = 0; i < array1.length; i++) {
-    let elem1 = array1[i];
-    let elem2 = array2[i];
+function concat(arg1, arg2) {
+
+    for (let i = 0; i < arg1.length; i++) {
+        let elem1 = arg1[i];
+        let elem2 = arg2[i];
 
 
-    arraySomma.push(elem1, elem2);
+        arraySomma.push(elem1, elem2);
+    }
+
 }
 
+
+concat(array1, array2);
 
 console.log('array finale: ', arraySomma);
